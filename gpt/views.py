6e8a6103ccc4c585cfea415b95d39563dfc0abf1
taeho0888 <taeho0888@naver.com>
2023-05-29 21:41:42 +0900

    session view 작성

--- gpt/views.py
+++ gpt/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from user.serializers import SessionSerializer
+from user.models import Session
 
-# Create your views here.
+
+class SessionViewSet(ModelViewSet):
+    queryset = Session.objects.all()
+    serializer_class = SessionSerializer
+
+    def get_user_id(self):
+        return self.get_object().userID
+
+    def get_session_id(self):
+        return self.get_object().sessionID
