1fb43ee7217a5b382508cfd84d2fb67f27b342f0
taeho0888 <taeho0888@naver.com>
2023-05-30 13:05:52 +0900

    session hashtag 모델 추가

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Session
+from .models import User, Session, Session_hashtag
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Session)
+admin.site.register(Session_hashtag)
--- user/models.py
+++ user/models.py
@@ -29,7 +29,8 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
 
-    username = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    username = models.CharField(
+        max_length=100, unique=True, null=False, blank=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -42,12 +43,23 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
-    
+
 
 class Session(models.Model):
     sessionID = models.BigAutoField(primary_key=True)
-    userID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='session')
-    subject = models.CharField(max_length=100)
+    userID = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='session')
     date = models.DateField()
     quiz = models.TextField(null=True, blank=True)
     summary = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.sessionID} (user: {self.userID})"
+
+
+class Session_hashtag(models.Model):
+    session = models.ManyToManyField(Session, related_name='hashtag')
+    hashtag = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.hashtag
