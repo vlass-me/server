8362ac405344feef66cc4b9ada0e146c853aec26
taeho0888 <taeho0888@naver.com>
2023-05-30 12:36:23 +0900

    sessionview에 permission_class 추가

--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .serializers import SessionSerializer
 from .models import Session
 
 # Create your views here.
+
+
 class SessionViewSet(ModelViewSet):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
+    permission_classes = (IsAuthenticated,)
